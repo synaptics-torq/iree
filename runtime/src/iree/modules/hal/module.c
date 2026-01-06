8187a020435e5139a2cac2bfad31d6b04ffbc1d6
ayounes-synaptics <50144496+ayounes-synaptics@users.noreply.github.com>
2026-01-06 15:06:43 +0100

    Double up the maximum descriptor binding count (#4)
    Moonshine decoder_with_past has 39 such binding.

--- runtime/src/iree/modules/hal/module.c
+++ runtime/src/iree/modules/hal/module.c
@@ -18,7 +18,7 @@
 
 // Limit the number of bindings we pass down through the HAL. This can be tuned
 // in the future but right now guards the stack from blowing up during calls.
-#define IREE_HAL_MODULE_MAX_DESCRIPTOR_BINDING_COUNT ((iree_host_size_t)32)
+#define IREE_HAL_MODULE_MAX_DESCRIPTOR_BINDING_COUNT ((iree_host_size_t)64)
 
 // Limit the number of execution bindings in a binding table. This today limits
 // our number of unique indirect buffers used within a command buffer but the
