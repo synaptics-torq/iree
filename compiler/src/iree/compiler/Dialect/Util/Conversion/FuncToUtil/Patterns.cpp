43014101c8abd478a4b24b7323519df153752783
Sauryadeep Pal <sauryadeep.pal@synaptics.com>
2026-06-08 05:47:39 -0700

    Fix operand tying (#29)
    * set anyTiedOperands correctly
    * propagate tied_operands through ABI

--- compiler/src/iree/compiler/Dialect/Util/Conversion/FuncToUtil/Patterns.cpp
+++ compiler/src/iree/compiler/Dialect/Util/Conversion/FuncToUtil/Patterns.cpp
@@ -52,6 +52,8 @@ struct FuncFuncOpPattern : public OpConversionPattern<func::FuncOp> {
           srcOp.getResultAttrOfType<IntegerAttr>(i, "iree.abi.tied");
       if (tiedAttr) {
         tiedOperands.push_back(tiedAttr.getInt());
+        // Synaptics change
+        anyTiedOperands = true;
       } else {
         tiedOperands.push_back(-1);
       }
